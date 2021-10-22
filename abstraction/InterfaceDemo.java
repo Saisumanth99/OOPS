6eeaa5a68a518deae0fada5bb40ecf77a9ec0dc2
Saisumanth99 <51310300+Saisumanth99@users.noreply.github.com>
2021-10-22 09:56:23 +0530

    interfaces implemented

---
+++ abstraction/InterfaceDemo.java
@@ -0,0 +1,48 @@
+package abstraction;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+interface RestAPI {
+    void post();
+    void get();
+    void put();
+    void delete();
+}
+
+class RestController implements RestAPI {
+
+    @Override
+    public void post() {
+        System.out.println("this method handles post request");
+        
+    }
+
+    @Override
+    public void get() {
+        System.out.println("this method handles get request");
+        
+    }
+
+    @Override
+    public void put() {
+        System.out.println("this method handles put request");
+        
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("this method handles delete request");
+        
+    }
+
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        RestController controller = new RestController();
+        controller.get();
+        controller.post();
+        controller.put();
+        controller.delete();
+    }
+}
